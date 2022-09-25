6ccc83a178ac962020434e4e05cd5208bf43122b
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-09-25 16:27:54 +0400

    :truck: Rearranges declarations

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import { Signal, signal } from '@preact/signals';
 import { StateUpdater, useCallback } from 'preact/hooks';
 
+const store: Record<string, Signal> = {};
+
+export const Store = (initialValues: Record<string, unknown>): void => {
+  Object.entries(initialValues).forEach(([key, value]) => {
+    store[key] = signal(value);
+  });
+};
+
+export const useGlobalSignal = <T>(key: string, defaultValue: T = null): Signal<T> => {
+  store[key] = store[key] ?? signal(defaultValue);
+  return store[key];
+};
+
 export const useGlobalState = <T>(key: string, defaultValue: T = null): [T, StateUpdater<T>] => {
   const signal = useGlobalSignal(key, defaultValue);
 
@@ -12,17 +25,4 @@ export const useGlobalState = <T>(key: string, defaultValue: T = null): [T, Stat
     [signal]
   );
   return [store[key].value, set];
-};
-
-export const useGlobalSignal = <T>(key: string, defaultValue: T = null): Signal<T> => {
-  store[key] = store[key] ?? signal(defaultValue);
-  return store[key];
-};
-
-export const store: Record<string, Signal> = {};
-
-export const Store = (initialValues: Record<string, unknown>): void => {
-  Object.entries(initialValues).forEach(([key, value]) => {
-    store[key] = signal(value);
-  });
 };
