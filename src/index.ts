4b6c483534f72f2b11dce6375ed562dd21889f6e
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-09-25 16:18:35 +0400

    :sparkles: Adds useGlobalSignal hook

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,30 @@
 import { Signal, signal } from '@preact/signals';
 import { StateUpdater, useCallback } from 'preact/hooks';
 
-export const useGlobalState: UseGlobalState = (key, defaultValue = null) => {
-  store[key] = store[key] ?? signal(defaultValue);
+export const useGlobalState = <T>(key: string, defaultValue: T = null): [T, StateUpdater<T>] => {
+  const signal = useGlobalSignal(key, defaultValue);
 
   const set = useCallback<StateUpdater<typeof defaultValue>>(
     (v) => {
-      const newValue = isFunction(v) && 'call' in v ? v(store[key].value) : v;
-      store[key].value = newValue;
+      const newValue = isFunction(v) && 'call' in v ? v(signal.value) : (v as T);
+      signal.value = newValue;
     },
-    [store[key]]
+    [signal]
   );
   return [store[key].value, set];
+};
+
+export const useGlobalSignal = <T>(key: string, defaultValue: T = null): Signal<T> => {
+  store[key] = store[key] ?? signal(defaultValue);
+  return store[key];
 };
 
 const isFunction = (v: any): boolean => typeof v === 'function';
 
 export const store: Record<string, Signal> = {};
 
-export const Store: StoreInitializer = (initialValues) => {
+export const Store = (initialValues: Record<string, unknown>): void => {
   Object.entries(initialValues).forEach(([key, value]) => {
     store[key] = signal(value);
   });
 };
-
-type UseGlobalState = <T>(key: string, defaultValue?: T) => [T, StateUpdater<T>];
-
-type StoreInitializer = (initialValues: Record<string, unknown>) => void;
--- test/globalState.spec.tsx
+++ test/globalState.spec.tsx
@@ -1,7 +1,8 @@
 import { h } from 'preact';
 import { render, fireEvent, screen, cleanup, renderHook, act } from '@testing-library/preact';
 import { afterEach, describe, expect, it } from 'vitest';
-import { useGlobalState } from '../src';
+import { useGlobalSignal, useGlobalState } from '../src';
+import { Signal } from '@preact/signals';
 
 describe('useGlobalState', () => {
   afterEach(cleanup);
@@ -65,5 +66,51 @@ describe('useGlobalState', () => {
     fireEvent.click(await screen.findByTestId('button-2'));
     expect((await screen.findByTestId('span-1')).textContent).toBe('2');
     expect((await screen.findByTestId('span-2')).textContent).toBe('2');
+  });
+});
+
+describe('useGlobalSignal', () => {
+  afterEach(cleanup);
+  it('should return a signal', () => {
+    const { result } = renderHook(() => useGlobalSignal('test', 12));
+    expect(result.current).toBeInstanceOf(Signal);
+    expect(result.current.value).toBeDefined();
+  });
+  it('should work return the same signal when same key provided', () => {
+    const { result: result1 } = renderHook(() => useGlobalSignal('multi', 12));
+    const { result: result2 } = renderHook(() => useGlobalSignal('multi', 12));
+    expect(result1.current).toBe(result2.current);
+  });
+  it('should return unique signals per key', () => {
+    const { result: result1 } = renderHook(() => useGlobalSignal('multi1', 12));
+    const { result: result2 } = renderHook(() => useGlobalSignal('multi2', 12));
+    expect(result1.current).not.toBe(result2.current);
+  });
+  it('should work in a component', async () => {
+    const Counter = ({ id }: { id: string }) => {
+      const count = useGlobalSignal('counter-signal', 1);
+      return (
+        <div>
+          <button data-testid={`button-${id}`} onClick={() => count.value++}>
+            +
+          </button>
+          <span data-testid={`span-${id}`}>{count}</span>
+        </div>
+      );
+    };
+    render(
+      <div>
+        <Counter id='1' />;
+        <Counter id='2' />;
+      </div>
+    );
+    expect((await screen.findByTestId('span-1')).textContent).toBe('1');
+    expect((await screen.findByTestId('span-2')).textContent).toBe('1');
+    fireEvent.click(await screen.findByTestId('button-1'));
+    expect((await screen.findByTestId('span-1')).textContent).toBe('2');
+    expect((await screen.findByTestId('span-2')).textContent).toBe('2');
+    fireEvent.click(await screen.findByTestId('button-2'));
+    expect((await screen.findByTestId('span-1')).textContent).toBe('3');
+    expect((await screen.findByTestId('span-2')).textContent).toBe('3');
   });
 });
